c6fd0127477a2bc32382551047a0be53b86d6274
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-04-01 18:22:01 +0200

    Update App.jsx

--- src/components/App.jsx
+++
@@ -1,5 +0,0 @@
-import { useEffect, useState } from "react";
-import { ContactForm } from "../contactForm/contactForm.jsx";
-import { SearchBox } from "../searchBox/searchBox.jsx";
-import { ContactList } from "../contactList/contactList.jsx";
-import contact from "./contact.json";
